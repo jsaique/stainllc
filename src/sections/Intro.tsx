221ddc738c1f9fbbdc8dd979f5b6683ef8eddc55
jsaique <jsaique11@gmail.com>
2025-05-14 13:52:02 -0400

    intro animation

--- src/sections/Intro.tsx
+++ src/sections/Intro.tsx
@@ -1,8 +1,38 @@
-import { FC } from "react";
+"use client";
+
+import { stagger, useAnimate, useInView } from "motion/react";
+import { FC, useEffect } from "react";
+import SplitType from "split-type";
 
 const Intro: FC = () => {
+  const [scope, animate] = useAnimate();
+  const inView = useInView(scope, {
+    once: true,
+  });
+
+  useEffect(() => {
+    new SplitType(scope.current.querySelector("h2"), {
+      types: "lines,words",
+      tagName: "span",
+    });
+  }, [scope]);
+
+  useEffect(() => {
+    if (inView) {
+      animate(
+        scope.current.querySelectorAll(".word"),
+        {
+          transform: "translateY(0%)",
+        },
+        {
+          duration: 0.5,
+          delay: stagger(0.2),
+        }
+      );
+    }
+  }, [inView, scope, animate]);
   return (
-    <section className="section md:16 lg:mt-20" id="intro">
+    <section className="section md:16 lg:mt-20" id="intro" ref={scope}>
       <div className="container">
         <h2 className="text-4xl md:text-6xl lg:text-7xl lg:w-[80%]">
           Established in 2005, we are a veteran-owned fabrication company based
